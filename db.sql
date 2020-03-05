20de7fb16701621e8a70568e4c26279ef58a13c7
Matthew Henschke <matthewhenschke98@gmail.com>
2020-03-05 08:53:12 -0500

    added middleware for stock, sql schema that I am using for app, and a txt file that enumerates the tech stack I used

---
+++ db.sql
@@ -0,0 +1,25 @@
+-- This is the db schema used for the database
+CREATE TABLE users(
+`id` int PRIMARY KEY AUTO_INCREMENT
+`name` VARCHAR(100) NOT NULL,
+`email` VARCHAR(100) NOT NULL UNIQUE,
+`balance` DECIMAL(10,2) NOT NULL DEFAULT 5000.00,
+`password` VARCHAR(100) NOT NULL /* USE SHA2 */
+);
+
+CREATE TABLE transactions(
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`user_id` INT NOT NULL REFERENCES Users(id),
+`stock_name` VARCHAR(30) NOT NULL,
+`unit_price` DECIMAL(15,2) NOT NULL,
+`qty` INT NOT NULL,
+`type` VARCHAR(4) NOT NULL,
+`timestamp` DATETIME NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE ownership(
+`user_id` INT NOT NULL REFERENCES Users(id),
+`stock_name` VARCHAR(30) NOT NULL,
+`qty` INT NOT NULL,
+PRIMARY KEY (userID, stock)
+);
