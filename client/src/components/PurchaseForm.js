6227c4644588ae92572420067c7a91d2c893505b
Matthew Henschke <matthewhenschke98@gmail.com>
2020-03-05 17:40:39 -0500

    added state to stock purchase form

--- client/src/components/PurchaseForm.js
+++ client/src/components/PurchaseForm.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 
 const PurchaseForm = () => {
+  const [ticker, setTicker] = useState(null);
+  const [qty, setQty] = useState(null);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
+
   return (
     <div className="mt-5 purchase-form text-center">
       <h2>Cash - $5,000</h2>
-      <Form className="mt-5">
+      <Form className="mt-5" onSubmit={handleSubmit}>
         <Form.Control
           type="text"
           className="mt-3"
           placeholder="Ticker"
+          onChange={e => setTicker(e.target.value)}
           required
         />
         <Form.Control
@@ -18,6 +26,7 @@ const PurchaseForm = () => {
           placeholder="Qty"
           min="1"
           required
+          onChange={e => setQty(e.target.value)}
         />
         <Button variant="primary" type="submit" className=" mt-3">
           Buy
