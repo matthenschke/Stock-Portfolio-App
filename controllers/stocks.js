b49f9dd57564e6fc83ba50599f75a13b1927e975
Matthew Henschke <matthewhenschke98@gmail.com>
2020-03-06 12:12:55 -0500

    added stock middleware to use IEX API for stock info and stocks routes

--- controllers/stocks.js
+++ controllers/stocks.js
@@ -1,5 +1,6 @@
 const express = require("express"); // express
-
 const router = express.Router(); // set up express router for authentication
+const { getStockInfo } = require("../middlewares/stocks");
 
+router.get("/:symbol", getStockInfo);
 module.exports = router;
--- middlewares/stocks.js
+++ middlewares/stocks.js
@@ -3,6 +3,15 @@ const axios = require("axios");
 module.exports = {
   getStockInfo: (req, res, next) => {
     const { symbol } = req.params;
-    //   axios.get(``)
+    axios
+      .get(
+        `https://cloud.iexapis.com/stable/stock/${symbol}/quote?token=${process.env.API_KEY}`
+      )
+      .then(response => {
+        res.status(200).json(response.data);
+      })
+      .catch(e => {
+        res.status(404).json("Not found");
+      });
   }
 };
