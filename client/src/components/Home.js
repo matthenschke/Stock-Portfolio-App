48027664cf21d6cb2c14d77b6b44cd9b6faeaf07
Matthew Henschke <matthewhenschke98@gmail.com>
2020-03-07 09:25:48 -0500

    modified ui for portfolio list so price is floated on right and moved api call to home so that I can wait for the purchase form and portfolio to load at the same time

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,15 +3,38 @@ import Navbar from "./Navbar";
 import Portfolio from "./Portfolio";
 import PurchaseForm from "./PurchaseForm";
 import auth from "../auth";
+import axios from "axios";
 
 const Home = () => {
-  return (
-    <>
-      <Navbar />
-      <Portfolio />
-      <PurchaseForm />
-    </>
-  );
+  const [stocks, setStocks] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const { id } = auth.activeUser;
+
+  useEffect(() => {
+    axios
+      .get(`/stocks/portfolio/${id}`)
+      .then(response => {
+        const { data: stocks } = response;
+        console.log(stocks);
+        setStocks(stocks);
+        setLoading(false);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }, [loading]);
+
+  if (!loading) {
+    return (
+      <>
+        <Navbar />
+        <Portfolio stocks={stocks} />
+        <PurchaseForm />
+      </>
+    );
+  } else {
+    return <></>;
+  }
 };
 
 export default Home;
--- client/src/components/Portfolio.js
+++ client/src/components/Portfolio.js
@@ -2,58 +2,39 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import auth from "../auth";
 
-const Portfolio = () => {
-  const [stocks, setStocks] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const { id } = auth.activeUser;
+const Portfolio = props => {
+  const { stocks } = props;
+  let portfolio = stocks.map(stock => {
+    console.log(stock);
+    const { ticker, qty, latestPrice, open: openingPrice } = stock;
+    let classes = "";
 
-  useEffect(() => {
-    axios
-      .get(`/stocks/portfolio/${id}`)
-      .then(response => {
-        const { data: stocks } = response;
-        console.log(stocks);
-        setStocks(stocks);
-        setLoading(false);
-      })
-      .catch(e => {
-        console.log(e);
-      });
-  }, [loading]);
-
-  if (!loading) {
-    let portfolio = stocks.map(stock => {
-      console.log(stock);
-      const { ticker, qty, latestPrice, open: openingPrice } = stock;
-      let classes = "";
-
-      if (openingPrice) {
-        if (openingPrice > latestPrice) {
-          classes = "text-danger";
-        } else if (openingPrice < latestPrice) {
-          classes = "text-success";
-        } else {
-          classes = "text-secondary";
-        }
+    if (openingPrice) {
+      if (openingPrice > latestPrice) {
+        classes = "text-danger";
+      } else if (openingPrice < latestPrice) {
+        classes = "text-success";
+      } else {
+        classes = "text-secondary";
       }
+    }
 
-      return (
-        <li key={ticker} className={`${classes}`}>
-          <span className={classes}></span>
-          {`${ticker} - ${qty} Shares @   ${latestPrice}`}
-          <hr />
-        </li>
-      );
-    });
     return (
-      <div className="portfolio">
-        <h1 className="text-center">Portfolio ($5943.34)</h1>
-        <ul className="ml-5 mt-5">{portfolio}</ul>
-      </div>
+      <li key={ticker} className={`${classes}`}>
+        <span className={classes}></span>
+        {`${ticker} - ${qty} Shares @`}
+        <span className="pl-4">{latestPrice}</span>
+        <hr />
+      </li>
     );
-  } else {
-    return <></>;
-  }
+  });
+
+  return (
+    <div className="portfolio">
+      <h1 className="text-center">Portfolio ($5943.34)</h1>
+      <ul className="ml-5 mt-5">{portfolio}</ul>
+    </div>
+  );
 };
 
 export default Portfolio;
